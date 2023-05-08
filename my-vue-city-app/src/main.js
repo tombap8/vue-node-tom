93d15384f761f3342adb2477a77ede20548a01bb
Tom B. Parker <tombap8@hotmail.com>
2023-05-08 12:28:11 +0900

    ss

--- my-vue-city-app/src/main.js
+++ my-vue-city-app/src/main.js
@@ -1,11 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+/* 뷰인스턴스 생성하기! */
 new Vue({
   el: '#app',
   components: { App },
