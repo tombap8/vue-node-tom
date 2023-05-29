d34b5adc10d03d29a7bdbcb4e96933aebb90da42
Tom B. Parker <tombap8@hotmail.com>
2023-05-29 11:06:37 +0900

    ss

---
+++ my-vue-city-app2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TopArea />
+    <MainArea />
+    <InfoArea />
+  </div>
+</template>
+
+<script>
+import TopArea from "./components/TopComp.vue";
+import MainArea from "./components/MainComp.vue";
+import InfoArea from "./components/InfoComp.vue";
+
+export default {
+  name: 'App',
+  components: {
+    TopArea,
+    MainArea,
+    InfoArea
+  }
+}
+</script>
+
+<style>
+#app {
+    width: 500px;
+    margin: 0 auto;
+}
+</style>
